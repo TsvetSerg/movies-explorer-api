8376d0afaf543f92e2ca90bc32776223d24f11d7
Sergay <tsvetkoffgolod@yandex.ru>
2022-02-24 15:47:52 +0300

    Database address setting final

--- app.js
+++ app.js
@@ -13,18 +13,14 @@ const {
   PORT = 3000,
   NODE_ENV,
   MONGO_URL,
+  DEFAULT_URL = 'mongodb://localhost:27017/moviesdb',
 } = process.env;
 
 const app = express();
 app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
 
-// mongoose.connect(NODE_ENV === 'production' ? MONGO_URL : 'mongodb://localhost:27017/moviesdb', {
-//   useUnifiedTopology: true,
-//   useNewUrlParser: true,
-// });
-
-mongoose.connect('mongodb://localhost:27017/moviesdb', {
+mongoose.connect(NODE_ENV === 'production' ? MONGO_URL : DEFAULT_URL, {
   useUnifiedTopology: true,
   useNewUrlParser: true,
 });
